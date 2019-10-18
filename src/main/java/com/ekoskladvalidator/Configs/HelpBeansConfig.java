c91d47795dffa38834c80ef27fc9aac4de1022cf
Ruslan Zosimov <ruslan543213@gmail.com>
2019-10-18 19:19:11 +0300

    LOGGINGstage configTR FIX

--- src/main/java/com/ekoskladvalidator/Configs/HelpBeansConfig.java
+++ src/main/java/com/ekoskladvalidator/Configs/HelpBeansConfig.java
@@ -1,13 +1,25 @@
 package com.ekoskladvalidator.Configs;
 
 import com.ekoskladvalidator.Handlers.RestErrorHandlers.RestTemplateResponseErrorHandler;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@ComponentScan
+@EnableScheduling
+@PropertySource("classpath:rest.properties")
 public class HelpBeansConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     @Bean
     public RestTemplate getRestTemplate() {
